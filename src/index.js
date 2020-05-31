5187f90c0f93887b6740aa9313be9ea421abbedb
Shahda <abdelrahmanshahda@yahoo.com>
2020-05-31 04:20:04 +0200

    Removing dayfrom the reservation endpoint

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const moment = require('moment-timezone')
 //app intialization
 const app = express()
 app.use(express.json())
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.get('/', (req, res) => {
     
 })
